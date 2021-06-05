8439e7db73d9651b41e7a643f91e53feb2b8ef93
Rishabh Devbanshi <rdevbanshi35@gmail.com>
2021-06-05 11:06:12 +0530

    fixed the search bar to search for titles also

--- js/app.js
+++ js/app.js
@@ -8,26 +8,48 @@ let addBtn = document.getElementById("addBtn");
 function showNotes() {
   let notes = localStorage.getItem("notes");
   let title = localStorage.getItem("title");
+  let important = localStorage.getItem("important")
+
   if (notes == null) {
     notesObj = [];
     notesTitle = [];
+    imp = [];
   } else {
     notesObj = JSON.parse(notes);
     notesTitle = JSON.parse(title);
+    imp = JSON.parse(important);
   }
 
   let html = "";
 
   notesObj.forEach((ele, idx) => {
-    html += `
-        <div class="card noteCard my-2 mx-2" style="width: 18rem;">
+    
+    if(imp != null && imp.includes(idx))
+    {
+      html += `
+        <div class="card noteCard my-2 mx-2" style="width: 20rem;" style = "background-color = red">
             <div class="card-body">
               <h5 class="card-title">${notesTitle[idx]}</h5>
               <p class="card-text">${ele}</p>
               <button id = "${idx}" onclick = "deleteNote(this.id)" class="btn btn-primary" id="delBtn">Delete Note</button>
+              <button id = "${idx}" onclick = "updateNote(this.id)" class="btn btn-primary updateButton" id="updtBtn">Mark Important</button>
             </div>
           </div>
                 `;
+    }
+    else
+    {
+    html += `
+        <div class="card noteCard my-2 mx-2" style="width: 20rem;">
+            <div class="card-body">
+              <h5 class="card-title">${notesTitle[idx]}</h5>
+              <p class="card-text">${ele}</p>
+              <button id = "${idx}" onclick = "deleteNote(this.id)" class="btn btn-primary" id="delBtn">Delete Note</button>
+              <button id = "${idx}" onclick = "updateNote(this.id)" class="btn btn-primary updateButton" id="updtBtn">Mark Important</button>
+            </div>
+          </div>
+                `;
+    }
   });
 
   let notesELm = document.getElementById("notes");
@@ -75,21 +97,73 @@ function deleteNote(idx) {
   //   console.log("Deleting in progress...");
   let notes = localStorage.getItem("notes");
   let title = localStorage.getItem("title");
+  let important = localStorage.getItem("important")
   if (notes == null) {
     notesObj = [];
     notesTitle = [];
+    imp = []
   } else {
     notesObj = JSON.parse(notes);
     notesTitle = JSON.parse(title);
+    imp = JSON.parse(important)
   }
 
   notesObj.splice(idx, 1);
   notesTitle.splice(idx, 1);
+
+  if(imp != null)
+  {
+  for(let i=0;i<imp.length;i++)
+  {
+    if(imp[i] == idx)
+    {
+      imp.splice(i,1);
+      break;
+    }
+  }
+}
+
   localStorage.setItem("notes", JSON.stringify(notesObj));
   localStorage.setItem("title", JSON.stringify(notesTitle));
+  localStorage.setItem("important",JSON.stringify(imp));
+
   showNotes();
   //   console.log("Completed Succesfully !");
 }
+
+//function to mark notes as important
+
+function updateNote(idx){
+  let notes = localStorage.getItem("notes");
+  let title = localStorage.getItem("title");
+  let important = localStorage.getItem("important")
+  if (notes == null) {
+    notesObj = [];
+    notesTitle = [];
+    imp = []
+  } else {
+    notesObj = JSON.parse(notes);
+    notesTitle = JSON.parse(title);
+    imp = JSON.parse(important)
+  }
+
+  let content = notesObj[idx]
+  let heading = notesTitle[idx]
+
+  deleteNote(idx);
+
+  notesObj.unshift(content);
+  notesTitle.unshift(heading);
+  imp.push(idx)
+
+  localStorage.setItem("notes", JSON.stringify(notesObj));
+  localStorage.setItem("title", JSON.stringify(notesTitle));
+  localStorage.setItem("important",JSON.stringify(imp));
+
+  showNotes();
+}
+
+// adding even listener to the search bar and searching for required results
 
 let searchTxt = document.getElementById("searchTxt");
 searchTxt.addEventListener("input", () => {
@@ -97,8 +171,9 @@ searchTxt.addEventListener("input", () => {
   let noteCard = document.getElementsByClassName("noteCard");
   Array.from(noteCard).forEach((ele) => {
     let cardTxt = ele.getElementsByTagName("p")[0].innerHTML.toLowerCase();
+    let cardTitle = ele.getElementsByTagName("h5")[0].innerHTML.toLowerCase();
 
-    if (cardTxt.includes(txt)) {
+    if (cardTxt.includes(txt) || cardTitle.includes(txt)) {
       ele.style.display = "block";
     } else {
       ele.style.display = "none";
